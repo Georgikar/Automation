7353aef8b65071c245f65f27859e8f263a233034
Georgi <georgi.karpovchanski@yahoo.com>
2022-01-10 19:37:18 +0200

    Adding Homework 8

--- src/main/java/Poly/Company.java
+++ src/main/java/Poly/Company.java
@@ -1,4 +1,36 @@
 package Poly;
 
 public class Company {
+
+    String nameofcompany;
+    String dateoffound;
+    String bulstat;
+
+
+
+
+    public String getNameofcompany() {
+        return nameofcompany;
+    }
+
+    public void setNameofcompany(String nameofcompany) {
+        this.nameofcompany = nameofcompany;
+    }
+
+    public String getDateoffound() {
+        return dateoffound;
+    }
+
+    public void setDateoffound(String dateoffound) {
+        this.dateoffound = dateoffound;
+    }
+
+    public String getBulstat() {
+        return bulstat;
+    }
+
+    public void setBulstat(String bulstat) {
+        if(bulstat.length() <= 10);
+        this.bulstat = bulstat;
+    }
 }
